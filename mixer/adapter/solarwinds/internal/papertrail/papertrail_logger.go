8ea20d7281d4c04bb425fff682796e8d579aad47
Girish Ranganathan <girish.ranganathan@solarwinds.com>
2018-01-10 19:44:07 -0500

    A better persist-to-disk & cleanup logic instead of memory

--- mixer/adapter/solarwinds/internal/papertrail/papertrail_logger.go
+++ mixer/adapter/solarwinds/internal/papertrail/papertrail_logger.go
@@ -19,15 +19,20 @@ import (
 	"html/template"
 	"log/syslog"
 	"net"
-	"regexp"
+	"os"
+	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 
+	"github.com/dgraph-io/badger"
 	"istio.io/istio/mixer/adapter/solarwinds/config"
 	"istio.io/istio/mixer/pkg/adapter"
+	"istio.io/istio/mixer/template/logentry"
+	"github.com/satori/go.uuid"
 	"istio.io/istio/mixer/pkg/pool"
 	"istio.io/istio/mixer/template/logentry"
 )
@@ -36,7 +41,12 @@ const (
 	defaultRetention = time.Duration(24 * time.Hour)
 	keyFormat        = "TS:%d-BODY:%s"
 	keyPattern       = "TS:(\\d+)-BODY:(.*)"
-	defaultTemplate  = `{{or (.originIp) "-"}} - {{or (.sourceUser) "-"}} ` +
+	defaultMaxDiskUsage = 5 // disk usage in percentage
+	defaultUltimateMaxDiskUsage = 99 // usage cannot go beyond this percentage value
+	defaultBatchSize = 1000 // records
+	dbLocation = "./badger"
+	cleanUpInterval = 5 * time.Second
+	defaultTemplate = `{{or (.originIp) "-"}} - {{or (.sourceUser) "-"}} ` +
 		`[{{or (.timestamp.Format "2006-01-02T15:04:05Z07:00") "-"}}] "{{or (.method) "-"}} {{or (.url) "-"}} ` +
 		`{{or (.protocol) "-"}}" {{or (.responseCode) "-"}} {{or (.responseSize) "-"}}`
 )
@@ -61,7 +71,11 @@ type Logger struct {
 
 	cmap *sync.Map
 
+	db *badger.DB
+
 	logInfos map[string]*logInfo
+
+	initialDiskUsage float64
 
 	log adapter.Logger
 
@@ -79,6 +93,15 @@ func NewLogger(paperTrailURL string, retention time.Duration, logConfigs map[str
 	if retention.Seconds() <= float64(0) {
 		retention = defaultRetention
 	}
+	opts := badger.DefaultOptions
+	opts.Dir = dbLocation
+	opts.ValueDir = dbLocation
+	//opts.ValueLogLoadingMode = options.FileIO
+
+	db, err := badger.Open(opts)
+	if err != nil {
+		return nil, logger.Errorf("Error: %v", err)
+	}
 
 	logger.Infof("Creating a new paper trail logger for url: %s", paperTrailURL)
 
@@ -90,6 +113,9 @@ func NewLogger(paperTrailURL string, retention time.Duration, logConfigs map[str
 		env:             env,
 		maxWorkers:      defaultWorkerCount * runtime.NumCPU(),
 		loopFactor:      true,
+
+		db:               db,
+		initialDiskUsage: diskUsage(),
 	}
 
 	p.logInfos = map[string]*logInfo{}
@@ -112,6 +138,9 @@ func NewLogger(paperTrailURL string, retention time.Duration, logConfigs map[str
 	}
 
 	env.ScheduleDaemon(p.flushLogs)
+
+	env.ScheduleDaemon(p.deleteExcess)
+	env.ScheduleDaemon(p.cleanup)
 	return p, nil
 }
 
@@ -136,14 +165,25 @@ func (p *Logger) Log(msg *logentry.Instance) error {
 	pool.PutBuffer(buf)
 
 	if len(payload) > 0 {
-		ut := time.Now().UnixNano()
-		p.cmap.Store(fmt.Sprintf(keyFormat, ut, payload), struct{}{})
+		if p.log.VerbosityLevel(config.DebugLevel) {
+			p.log.Infof("AO - In Log method. Now persisting log: %s", msg)
+		}
+		uuid := uuid.NewV4()
+		err := p.db.Update(func(txn *badger.Txn) error {
+			err := txn.SetWithTTL([]byte(fmt.Sprintf(keyFormat, time.Now().UnixNano(), uuid)), []byte(payload), p.retentionPeriod)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+		if err != nil {
+			return p.log.Errorf("Error: ", err)
+		}
 	}
 	return nil
 }
 
 func (p *Logger) sendLogs(data string) error {
-	var err error
 	writer, err := syslog.Dial("udp", p.paperTrailURL, syslog.LOG_EMERG|syslog.LOG_KERN, "istio")
 	if err != nil {
 		return p.log.Errorf("ao - Failed to dial syslog: %v", err)
@@ -158,47 +198,116 @@ func (p *Logger) sendLogs(data string) error {
 
 // This should be run in a routine
 func (p *Logger) flushLogs() {
-	var err error
-
-	re := regexp.MustCompile(keyPattern)
-
 	for p.loopFactor {
-		hose := make(chan interface{}, p.maxWorkers)
+		hose := make(chan []byte, p.maxWorkers)
 		var wg sync.WaitGroup
 
 		// workers
 		for i := 0; i < p.maxWorkers; i++ {
 			go func(worker int) {
-				for keyI := range hose {
-					key, _ := keyI.(string)
-					match := re.FindStringSubmatch(key)
-					if len(match) > 2 {
-						err = p.sendLogs(match[2]) // which is the actual log msg
+				if p.log.VerbosityLevel(config.DebugLevel) {
+					p.log.Infof("AO - flushlogs, worker %d initialized.", (worker + 1))
+					defer p.log.Infof("AO - flushlogs, worker %d signing off.", (worker + 1))
+				}
+
+				for key := range hose {
+					if p.log.VerbosityLevel(config.DebugLevel) {
+						p.log.Infof("AO - flushlogs, worker %d took the job.", (worker + 1))
+					}
+
+					err := p.db.Update(func(txn *badger.Txn) error {
+						item, err := txn.Get(key)
+						if err != nil {
+							if err == badger.ErrKeyNotFound {
+								return nil
+							} else {
+								return err
+							}
+						}
+						var val []byte
+						val, err = item.ValueCopy(val)
+						if err != nil {
+							if err == badger.ErrKeyNotFound {
+								return nil
+							} else {
+								return err
+							}
+						}
+						err = p.sendLogs(string(val))
 						if err == nil {
-							p.cmap.Delete(key)
-							wg.Done()
-							continue
+							if p.log.VerbosityLevel(config.DebugLevel) {
+								p.log.Infof("AO - flushLogs, delete key: %s", key)
+							}
+							err := txn.Delete(key)
+							if err != nil {
+								return err
+							}
+							return nil
 						}
-
-						tsN, _ := strconv.ParseInt(match[1], 10, 64)
-						ts := time.Unix(0, tsN)
-
-						if time.Since(ts) > p.retentionPeriod {
-							p.cmap.Delete(key)
-						}
+						return nil
+					})
+					if err != nil {
+						p.log.Errorf("Error while deleting key: %s - error: %v", key, err)
 					}
 					wg.Done()
 				}
 			}(i)
 		}
 
-		p.cmap.Range(func(k, v interface{}) bool {
-			wg.Add(1)
-			hose <- k
-			return true
+		err := p.db.View(func(txn *badger.Txn) error {
+			opts := badger.DefaultIteratorOptions
+			opts.PrefetchValues = false
+			it := txn.NewIterator(opts)
+			defer it.Close()
+			for it.Rewind(); it.Valid(); it.Next() {
+				item := it.Item()
+				k := make([]byte, len(item.Key()))
+				copy(k, item.Key())
+				wg.Add(1)
+				hose <- k
+			}
+			return nil
 		})
+		if err != nil {
+			p.log.Errorf("AO - flush logs - Error reading keys from db: ", err)
+		}
 		wg.Wait()
 		close(hose)
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func (p *Logger) deleteExcess() {
+	for p.loopFactor {
+		currentUsage := diskUsage()
+		if p.log.VerbosityLevel(config.DebugLevel) {
+			p.log.Infof("Current disk usage: %.2f %%", currentUsage)
+			p.log.Infof("DB folder size: %.2f MB", computeDirectorySizeInMegs(dbLocation))
+		}
+		if currentUsage > p.initialDiskUsage+defaultMaxDiskUsage || currentUsage > defaultUltimateMaxDiskUsage {
+			// delete from beginning
+			iterations := defaultBatchSize
+			err := p.db.View(func(txn *badger.Txn) error {
+				opts := badger.DefaultIteratorOptions
+				opts.PrefetchValues = false
+				it := txn.NewIterator(opts)
+				defer it.Close()
+				for it.Rewind(); it.Valid(); it.Next() {
+					item := it.Item()
+					k := make([]byte, len(item.Key()))
+					copy(k, item.Key())
+					txn.Delete(k)
+					iterations--
+					if iterations < 0 {
+						break
+					}
+				}
+				return nil
+			})
+			if err != nil {
+				p.log.Errorf("AO - deleteExcess - Error while deleting - error: %v", err)
+			}
+		}
 		time.Sleep(500 * time.Millisecond)
 	}
 }
@@ -206,5 +315,53 @@ func (p *Logger) flushLogs() {
 // Close - closes the Logger instance
 func (p *Logger) Close() error {
 	p.loopFactor = false
+	time.Sleep(time.Second)
+	if p.db != nil {
+		return p.db.Close()
+	}
 	return nil
+}
+
+func (p *Logger) cleanup() {
+	for p.loopFactor {
+		if p.db != nil {
+			if p.log.VerbosityLevel(config.DebugLevel) {
+				p.log.Infof("AO - cleanup - running GC")
+			}
+			p.db.PurgeOlderVersions()
+			p.db.RunValueLogGC(0.99)
+		}
+		time.Sleep(cleanUpInterval)
+	}
+}
+
+func parseRetention(logRetentionStr string) time.Duration {
+	retention, err := time.ParseDuration(logRetentionStr)
+	if err != nil {
+		retention, _ = time.ParseDuration(defaultRetention)
+	}
+	if retention.Seconds() <= float64(0) {
+		retention, _ = time.ParseDuration(defaultRetention)
+	}
+	return retention
+}
+
+func diskUsage() float64 {
+	var stat syscall.Statfs_t
+	wd, _ := os.Getwd()
+	syscall.Statfs(wd, &stat)
+	avail := stat.Bavail * uint64(stat.Bsize)
+	used := stat.Blocks * uint64(stat.Bsize)
+	return (float64(used) / float64(used+avail)) * 100
+}
+
+func computeDirectorySizeInMegs(fullPath string) float64 {
+	var sizeAccumulator int64
+	filepath.Walk(fullPath, func(path string, file os.FileInfo, err error) error {
+		if !file.IsDir() {
+			atomic.AddInt64(&sizeAccumulator, file.Size())
+		}
+		return nil
+	})
+	return float64(atomic.LoadInt64(&sizeAccumulator)) / (1024 * 1024)
 }
